54f9b25f04cbf2a90af20fef0e5628bf734f3f38
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-02-26 12:04:22 -0500

    16 passed

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * YOUR CODE HERE *
  ******************/
 function slice (str, returnStrfromThatIndexNum, returnStrEmptyBeyondlengthOfStrNum) {
-  if (str === true && typeof returnStrfromThatIndexNum === 'undefined' && typeof returnStrEmptyBeyondlengthOfStrNum === 'undefined') {
+  if (str !== undefined && typeof returnStrfromThatIndexNum === 'undefined' && typeof returnStrEmptyBeyondlengthOfStrNum === 'undefined') {
     return str;
   } else if (str && typeof returnStrfromThatIndexNum === 'number' && typeof returnStrEmptyBeyondlengthOfStrNum === 'undefined') {
     return str.substring(returnStrfromThatIndexNum); 
@@ -12,7 +12,16 @@ function slice (str, returnStrfromThatIndexNum, returnStrEmptyBeyondlengthOfStrN
     return str;
   }
   }
+/*
 
+function includes (arr, hold){
+  for (let i = 0; i < arr.length; i ++){
+    if(arr[i] === hold){
+      return true;}
+  }
+return false;
+} 
+*/
 function repeat (str, repeatStrNum) {
 let intermediateEmptyStr = '';
 for (i = 0; i < repeatStrNum; i++) {
@@ -27,9 +36,12 @@ function startsWith (firstGivenStr, secondGivenStr) {
 //can tell whether the first given string starts with the second given string when the strings are identical
     if (firstGivenStr === secondGivenStr) {
       return true;
-    } else if (firstGivenStr.charAt(0) === secondGivenStr.charAt(0) && secondGivenStr.length === 1) {
+    } else if (firstGivenStr.charAt(0) === secondGivenStr.charAt(0)
+    && secondGivenStr.length === 1) {
       return true;
-    } else if (firstGivenStr.charAt(0) === secondGivenStr.charAt(0) && secondGivenStr.length > 1 && firstGivenStr.charAt(1) === secondGivenStr.charAt(1)) {
+    } else if (firstGivenStr.charAt(0) === secondGivenStr.charAt(0)
+    && secondGivenStr.length > 1
+    && firstGivenStr.charAt(1) === secondGivenStr.charAt(1)) {
         return true;
     } else {
       return false;
@@ -38,27 +50,75 @@ function startsWith (firstGivenStr, secondGivenStr) {
 
 
 
-function endsWith () {
+function endsWith (firstGivenStr, secondGivenStr) {
+  for (let i =  0; i < secondGivenStr.length - 1; i++) {
+    if (firstGivenStr.charAt(i) === secondGivenStr.charAt(i)
+    && firstGivenStr.charAt(firstGivenStr.length - 1) === secondGivenStr.charAt(secondGivenStr.length - 1)) {
+      return true;
+    } else {
+      return false;
+    }
+  }  
+  if (firstGivenStr === secondGivenStr) {
+      return true;
+    } else if (firstGivenStr.charAt(firstGivenStr.length-1) === secondGivenStr.charAt(0) 
+    && secondGivenStr.length === 1) {
+      return true;
+    // } else if (firstGivenStr.charAt(firstGivenStr.length-1) === secondGivenStr.charAt(firstGivenStr.length-1)
+    // && firstGivenStr.charAt(firstGivenStr.length-2) === secondGivenStr.charAt(firstGivenStr.length-2) 
+    // && firstGivenStr.charAt(firstGivenStr.length-3) === secondGivenStr.charAt(firstGivenStr.length-3)
+    // && secondGivenStr.length > 1) {
+    //   return true;
+    } else {
+      return false;
+    }
+    }
+    
+
+
+function includes (array, GivenItem) {
+for (let i = 0; i < array.length; i++) {
+  if (array[i] === GivenItem) {
+    return true;
+  }
+} return false;
+}
+
+function join (strArray, mightySeparator) {
+  let emptyStr = '';
+  for (let i = 0; i < strArray.length; i++) {
+    if (mightySeparator && i !== strArray.length-1) {
+      emptyStr += strArray[i] + mightySeparator;
+    } else {
+      emptyStr += strArray[i];
+    }
+  }
+  return emptyStr;
+}
+
+
+function split (str, mightySeparator) {
+  const emptyArray = [];
+  if (str !== undefined && mightySeparator === undefined) {
+    emptyArray.push(str);
+
+    return emptyArray;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+  }
+
+  return emptyArray;
+}
+
+split('hello')
+
+
+function trimStart (str) {
 
 }
 
-function includes () {
-
-}
-
-function join () {
-
-}
-
-function split () {
-
-}
-
-function trimStart () {
-
-}
-
-function trimEnd () {
+function trimEnd (str) {
 
 }
 
