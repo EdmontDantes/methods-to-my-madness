eec0824a713964efd2737b2a96f152d0ba0ba418
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-02-25 19:30:58 -0500

    first 8 passed, slice, repeat, startsWith (this startsWith not able to fully understand what the questions are still asking me

--- main.js
+++ main.js
@@ -1,10 +1,66 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function slice (str, returnStrfromThatIndexNum, returnStrEmptyBeyondlengthOfStrNum) {
+  if (str === true && typeof returnStrfromThatIndexNum === 'undefined' && typeof returnStrEmptyBeyondlengthOfStrNum === 'undefined') {
+    return str;
+  } else if (str && typeof returnStrfromThatIndexNum === 'number' && typeof returnStrEmptyBeyondlengthOfStrNum === 'undefined') {
+    return str.substring(returnStrfromThatIndexNum); 
+  } else if (str && typeof returnStrfromThatIndexNum === 'number' && typeof returnStrEmptyBeyondlengthOfStrNum === 'number') {
+    return str.substring(returnStrfromThatIndexNum, returnStrEmptyBeyondlengthOfStrNum);
+  } else {
+    return str;
+  }
+  }
+
+function repeat (str, repeatStrNum) {
+let intermediateEmptyStr = '';
+for (i = 0; i < repeatStrNum; i++) {
+  intermediateEmptyStr = intermediateEmptyStr + str;
+}
+return intermediateEmptyStr;
+}
+
+function startsWith (firstGivenStr, secondGivenStr) {
+//can tell whether the first given string starts with the second single-character string
+//can tell whether the first given string starts with the second milti-character string
+//can tell whether the first given string starts with the second given string when the strings are identical
+    if (firstGivenStr === secondGivenStr) {
+      return true;
+    } else if (firstGivenStr.charAt(0) === secondGivenStr.charAt(0) && secondGivenStr.length === 1) {
+      return true;
+    } else if (firstGivenStr.charAt(0) === secondGivenStr.charAt(0) && secondGivenStr.length > 1 && firstGivenStr.charAt(1) === secondGivenStr.charAt(1)) {
+        return true;
+    } else {
+      return false;
+    }
+}
 
 
 
+function endsWith () {
 
+}
+
+function includes () {
+
+}
+
+function join () {
+
+}
+
+function split () {
+
+}
+
+function trimStart () {
+
+}
+
+function trimEnd () {
+
+}
 
 
 /*********************************
